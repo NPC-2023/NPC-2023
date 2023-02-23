b88ab4f2548767d9bda25b1265d8209fc8de473b
sondahyun <88223753+sondahyun@users.noreply.github.com>
2023-02-23 16:21:24 +0900

    d

--- main.lua
+++ main.lua
@@ -13,7 +13,7 @@ local physics = require "physics"
 physics.start()
 -- event listeners for tab buttons:
 local function onFirstView( event )
-	composer.gotoScene( "view01" )
+	composer.gotoScene( "view02_fall_game" )
 end
 
 onFirstView()	-- invoke first tab button's onPress event manually
