d00f516e53eff1df50afab5e1756580f6b773006
sondahyun <88223753+sondahyun@users.noreply.github.com>
2023-02-25 18:26:43 +0900

    d

--- view05_main_map.lua
+++ view05_main_map.lua
@@ -13,12 +13,131 @@ local json = require( "json" )
 function scene:create( event )
 	local sceneGroup = self.view
 	
+	local loadedSettings = loadsave.loadTable( "settings.json" )
+    local loadedEndings = loadsave.loadTable( "endings.json" )
+
+
 	local background = display.newImageRect("image/지도/background.png", display.contentWidth, display.contentHeight)
 	background.x, background.y=display.contentWidth/2, display.contentHeight/2
-
-	
 	sceneGroup:insert(background)
 	
+	local foundation1 = display.newImageRect("image/게임시작/새게임.png", 200, 80)
+    foundation1.x,foundation1.y = display.contentWidth * 0.1, display.contentHeight * 0.8
+    foundation1.name = "1"
+    sceneGroup:insert(foundation1)
+
+    local foundation2 = display.newImageRect("image/게임시작/새게임.png", 200, 80)
+    foundation2.x,foundation2.y = display.contentWidth * 0.3, display.contentHeight * 0.8
+    foundation2.name = "2"
+    sceneGroup:insert(foundation2)
+
+    local foundation3 = display.newImageRect("image/게임시작/새게임.png", 200, 80)
+    foundation3.x,foundation3.y = display.contentWidth * 0.5, display.contentHeight * 0.8
+    foundation3.name = "3"
+    sceneGroup:insert(foundation3)
+
+    local foundation4 = display.newImageRect("image/게임시작/새게임.png", 200, 80)
+    foundation4.x,foundation4.y = display.contentWidth * 0.7, display.contentHeight * 0.8
+    foundation4.name = "4"
+    sceneGroup:insert(foundation4)
+
+
+	
+	
+
+
+-- 마을 맵 마을 객체 생성.
+	local click1 = audio.loadStream( "music/스침.wav" )
+	
+-- 마을 객체 커서 범위 설정. 범위 밖으로 나가면 마을 크기 작아지고 안으로 들어가면 마을 크기 커짐.
+	local i = 0
+	local function bigbig (event)
+		
+		if (event.target.x-event.x)^2 + (event.target.y-event.y)^2 < 110^2 then
+			-- i값을 지정해 놓는 이유는 범위 안에서는 크기가 더 늘어나거나 줄어들지 않고, 소리가 연이어 나오지 않음.
+			if i == 0 then
+				local backgroundMusicChannel = audio.play(click1)
+				event.target.width = event.target.width*1.1
+				event.target.height = event.target.height*1.1
+				i = i + 1
+			end
+		
+		elseif (event.target.x-event.x)^2 + (event.target.y-event.y)^2 > 110^2 then
+			if i == 1 then
+				event.target.width =event.target.width/11*10
+				event.target.height =event.target.height/11*10
+				i = i - 1 
+			end
+			
+		end
+	end
+
+	local color = 0
+
+	
+-- 리스너 함수 생성
+	local function touch_ui (event)
+		if event.phase == "began" then
+			color = event.target.name
+
+			if color == "6" then
+				local click01 = audio.play(click1)
+				local home = audio.loadStream( "music/Trust.mp3" )
+				audio.setVolume( loadedEndings.logValue )
+				audio.play(home)
+				loadedEndings.bgMusic = "music/Trust.mp3"
+        		loadsave.saveTable(loadedEndings,"endings.json")
+				composer.removeScene("view01Map")
+				composer.gotoScene( "view00Room" )
+
+			elseif color == "7" then
+				local click01 = audio.play(click1)
+				local storeMusic = audio.loadStream( "music/Trust.mp3" )
+				audio.setVolume( loadedEndings.logValue )
+				audio.play(storeMusic);
+				loadedEndings.bgMusic = "music/Trust.mp3"
+        		loadsave.saveTable(loadedEndings,"endings.json")
+				composer.removeScene("view1Map")
+				composer.gotoScene( "view04Store" )
+
+			elseif color == "8" then
+				local click01 = audio.play(click1)
+				local storeMusic = audio.loadStream( "music/Trust.mp3" )
+				audio.setVolume( loadedEndings.logValue )
+				audio.play(storeMusic);
+				loadedEndings.bgMusic = "music/Trust.mp3"
+        		loadsave.saveTable(loadedEndings,"endings.json")
+				composer.removeScene("view1Map")
+				composer.gotoScene( "view04Deco" )
+
+			else
+				local click01 = audio.play(click1)
+				composer.setVariable("color", color)
+				composer.removeScene("view1Map")
+				composer.gotoScene("view02Map")
+
+			end
+		end
+	end
+
+-- 리스너 추가
+	foundation1:addEventListener("mouse",bigbig)
+	foundation2:addEventListener("mouse",bigbig)
+	foundation3:addEventListener("mouse",bigbig)
+	foundation4:addEventListener("mouse",bigbig)
+
+	foundation1:addEventListener("touch",touch_ui)
+	foundation2:addEventListener("touch",touch_ui)
+	foundation3:addEventListener("touch",touch_ui)
+	foundation4:addEventListener("touch",touch_ui)
+
+
+
+
+
+
+
+
 
 	-- showoverlay 함수 사용 option
     local options = {
@@ -36,6 +155,7 @@ function scene:create( event )
     --샘플 볼륨 이미지
     local volumeButton = display.newImageRect("image/설정/설정.png", 100, 100)
     volumeButton.x,volumeButton.y = display.contentWidth * 0.91, display.contentHeight * 0.4
+    
     sceneGroup:insert(volumeButton)
 
 
