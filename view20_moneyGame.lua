31c7c6a410fb0eb1c064e616e0c6f50003029fa3
mk-star <ssunn0812@naver.com>
2023-07-01 12:28:42 +0900

    노래변경

--- view20_moneyGame.lua
+++ view20_moneyGame.lua
@@ -70,7 +70,7 @@ function scene:create( event )
 	local buy = display.newText("계산하기", display.contentWidth*0.5, display.contentHeight*0.7, "font/DOSGothic.ttf", 50)
 	buy:setFillColor(1)
 
- 	local home = audio.loadStream( "music/music14.mp3" )
+ 	local home = audio.loadStream( "music/music14.wav" )
     audio.setVolume( loadedEndings.logValue )--loadedEndings.logValue
 
     local musicOption = { 
