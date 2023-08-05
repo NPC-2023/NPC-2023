3c9fdd7cf9358bd7196123432a401421a0e30374
mk-star <ssunn0812@naver.com>
2023-08-05 22:37:43 +0900

    우체통 사이즈 조절

--- openPostbox.lua
+++ openPostbox.lua
@@ -28,31 +28,32 @@ function scene:create( event )
     black:setFillColor(0)
     sceneGroup:insert(black)
 
-    local p = display.newImageRect("image/postbox/우체통클릭화면.png", 750, 750)
-    p.x, p.y = display.contentWidth/2, display.contentHeight/2
+    local p = display.newImageRect("image/postbox/우체통클릭화면.png", display.contentWidth*1.6, display.contentHeight*1.05)
+    p.x, p.y = display.contentCenterX, display.contentCenterY*1.2
     sceneGroup:insert(p)
    
     local c_box = {}
     local objectGroup = display.newGroup()
     for i=1, 6 do 
-        c_box[i] = display.newImageRect("image/postbox/옷 커스텀창.png", 100, 100)
+        c_box[i] = display.newImageRect("image/postbox/옷 커스텀창.png", display.contentWidth/4.5, display.contentHeight/7)
         c_box[i].alpha = 0
         objectGroup:insert(c_box[i])
     end
 
     local clothes = {}
-    clothes[1] = display.newImageRect("image/postbox/outer4.png", 80, 80) --봄옷
+    clothes[1] = display.newImageRect("image/postbox/outer4.png", display.contentWidth/5.5, display.contentHeight/9) --봄옷
     clothes[1].name = "outer4"
-    clothes[2] = display.newImageRect("image/postbox/outer5.png", 80, 80) --여름옷1
+    clothes[2] = display.newImageRect("image/postbox/outer5.png", display.contentWidth/5.5, display.contentHeight/9) --여름옷1
     clothes[2].name = "outer5"
-    clothes[3] = display.newImageRect("image/postbox/outer6.png", 80, 80) --여름옷2
+    clothes[3] = display.newImageRect("image/postbox/outer6.png", display.contentWidth/5.5, display.contentHeight/9) --여름옷2
     clothes[3].name = "outer6"
-    clothes[4] = display.newImageRect("image/postbox/outer7.png", 80, 80) --가을옷
+    clothes[4] = display.newImageRect("image/postbox/outer7.png", display.contentWidth/5.5, display.contentHeight/9) --가을옷
     clothes[4].name = "outer7"
-    clothes[5] = display.newImageRect("image/postbox/outer8.png", 80, 80) --겨울옷1
+    clothes[5] = display.newImageRect("image/postbox/outer8.png", display.contentWidth/5.5, display.contentHeight/9) --겨울옷1
     clothes[5].name = "outer8"
-    clothes[6] = display.newImageRect("image/postbox/outer9.png", 80, 80) --겨울옷2
+    clothes[6] = display.newImageRect("image/postbox/outer9.png", display.contentWidth/5.5, display.contentHeight/9) --겨울옷2
     clothes[6].name = "outer9"
+    
     for i=1, 6 do 
         objectGroup:insert(clothes[i])
          clothes[i].alpha = 0
@@ -60,14 +61,16 @@ function scene:create( event )
     sceneGroup:insert(objectGroup)
 
     local p_cnt = 1
+
     for i=1, 6 do
         print(loadedSettings.clothes[i])
+        loadedSettings.clothes[i] = true
         if(loadedSettings.clothes[i] == true) then --히든 퀘스트 4개 완. 옷을 받았다 = true로 바꿈
             print("요"..i.."!")
             c_box[i].alpha = 1
-            c_box[i].x , c_box[i].y = display.contentWidth*0.32+(p_cnt-1)*100, display.contentHeight/2 -140
+            c_box[i].x , c_box[i].y = (p_cnt-1)*67, p.y/1.5
             clothes[i].alpha = 1
-            clothes[i].x, clothes[i].y = c_box[i].x, c_box[i].y
+            clothes[i].x, clothes[i].y = c_box[i].x, c_box[i].y-2
             p_cnt = p_cnt + 1
         end
     end
@@ -108,9 +111,10 @@ function scene:create( event )
     end
 
     -- exit 버튼 생성 및 버튼에 이벤트 리스너 추가
-    local exit = display.newImageRect("image/설정/닫기.png", 60, 60)
+    local exit = display.newImageRect("image/설정/닫기.png", display.contentWidth/7.5, display.contentHeight/13)
+    exit.x, exit.y = display.contentCenterX*2.25, display.contentCenterY*0.5
     sceneGroup:insert(exit)
-    exit.x, exit.y = display.contentWidth*0.73, display.contentHeight*0.16
+    
    
     exit:addEventListener("touch",goback)
     for i=1,6 do
